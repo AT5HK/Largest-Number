8cc5c513584bacffd1ff285f6a6f08311f94aede
Auston Salvana <auston.salvana@live.com>
2015-06-18 19:33:02 -0700

    first commit

---
+++ Largest Number/Find.h
@@ -0,0 +1,13 @@
+//
+//  Find.h
+//  Largest Number
+//
+//  Created by Auston Salvana on 6/18/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Find : NSObject
+
+@end
---
+++ Largest Number/Find.m
@@ -0,0 +1,13 @@
+//
+//  Find.m
+//  Largest Number
+//
+//  Created by Auston Salvana on 6/18/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import "Find.h"
+
+@implementation Find
+
+@end
